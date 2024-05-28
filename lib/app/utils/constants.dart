0b15a8d3265c92636e8680c748484e6042fa5760
Carlos Parra <altrya-dev@hotmail.com>
2024-05-28 18:41:33 +0200

    put current ip

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static String currentIp = "34.42.129.51";
+  static String currentIp = "34.30.227.243";
   static String currentPort = "5000";
   static String getCompleteUrl =
       "http://${Constants.currentIp}:${Constants.currentPort}/api/v1/";
